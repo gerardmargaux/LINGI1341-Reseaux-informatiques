a1c95930bb6d526003c78afe2774f86461977bc3
François De Keersmaeker <francoisdekeersmaeker8@gmail.com>
2018-10-10 09:31:48 +0200

    créé sender et receiver

---
+++ receiver.c
@@ -0,0 +1,26 @@
+// @Titre : Projet LINGI1341 : Réseaux informatiques
+// @Auteurs : Francois DE KEERSMAEKER (7367 1600) & Margaux GERARD (7659 1600)
+// @Date : 22 octobre 2018
+
+/*
+ * Receiver : programme qui reçoit des paquets de données depuis le réseau.
+ *
+ */
+
+
+/*
+ * main : Fonction principale
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+int main(int argc, char *argv[]) {
+
+}
+
---
+++ sender.c
@@ -0,0 +1,25 @@
+// @Titre : Projet LINGI1341 : Réseaux informatiques
+// @Auteurs : Francois DE KEERSMAEKER (7367 1600) & Margaux GERARD (7659 1600)
+// @Date : 22 octobre 2018
+
+/*
+ * Sender : programme qui envoie des paquets de données sur le réseau.
+ *
+ */
+
+/*
+ * main : Fonction principale
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+int main(int argc, char *argv[]) {
+  print("Coucou j'envoie depuis Atom", );
+
+}
