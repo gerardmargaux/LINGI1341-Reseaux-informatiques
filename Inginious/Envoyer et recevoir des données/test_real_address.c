93caafafdaa42a75277a2677f4f548135320aaa1
François De Keersmaeker <francoisdekeersmaeker8@gmail.com>
2018-10-14 13:25:24 +0200

    Test real_address

--- Inginious/Envoyer et recevoir des données/test_real_address.c
+++ Inginious/Envoyer et recevoir des données/test_real_address.c
@@ -1,3 +1,4 @@
+#include "real_address.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -8,19 +9,24 @@
 #include <netinet/in.h>
 
 const char *real_address(const char *address, struct sockaddr_in6 *rval){
+  
     int err;
-    struct addrinfo hints;
+
+    struct addrinfo *hints = (struct addrinfo *) calloc(1, sizeof(struct addrinfo));
+    if(hints == NULL){
+      return "Erreur d'alloction de mémoire.";
+    }
     struct addrinfo *results;
 
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET6;
-    hints.ai_protocol = IPPROTO_UDP;
-		hints.ai_socktype = SOCK_DGRAM; // Car UDP packets
-		// hints.ai_socktype = SOCK_STREAM si TCP packets
+    //memset(hints, 0, sizeof(hints)); inutile si on fait calloc() au-dessus
+    hints->ai_family = AF_INET6;
+    hints->ai_protocol = IPPROTO_UDP;
+		hints->ai_socktype = SOCK_DGRAM; // Car UDP packets
+		// hints.ai_socktype = SOCK_STREAM; si TCP packets
 
-    err = getaddrinfo(address, "http", &hints, &results);
+    err = getaddrinfo(address, "http", hints, &results);
     if(err != 0){
-        return "Erreur d'exécution de la fonction addrinfo";
+        return "Erreur d'exécution de la fonction addrinfo.";
     }
 
     struct sockaddr *temp = results->ai_addr;
@@ -33,7 +39,13 @@ const char *real_address(const char *address, struct sockaddr_in6 *rval){
 
 // Teste la fonction real_address()
 int main(int argc, char* argv[]){
-  int err;
+
+  char* err = (char*) malloc(100*sizeof(char));
+  if(err == NULL){
+    fprintf(stderr, "ERROR : fonction malloc()\n");
+    return -1;
+  }
+
   char* address = argv[1];
 
   struct sockaddr_in6 *rval = (struct sockaddr_in6 *) malloc(sizeof(struct sockaddr_in6));
@@ -43,7 +55,13 @@ int main(int argc, char* argv[]){
   }
 
   err = real_address(address, rval);
-  char* net_address = (rval->sin6.addr).s6_addr;
+  if(err != NULL){
+    fprintf(stderr, "%s\n", err);
+    free(err);
+    free(rval);
+    return -1;
+  }
+  char* net_address = (rval->sin6_addr).s6_addr;
   char* printable_address = (char*) malloc(100*sizeof(char));
   if(printable_address == NULL){
     fprintf(stderr, "ERROR : fonction malloc()\n");
