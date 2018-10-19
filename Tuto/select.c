824335bc59b96a739533d74171e9226cdb238f41
François De Keersmaeker <francoisdekeersmaeker8@gmail.com>
2018-10-19 21:15:12 +0200

    Test select

--- Tuto/select.c
+++ Tuto/select.c
@@ -6,27 +6,50 @@
 #include <sys/time.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <sys/select.h>
+#include <string.h>
 
 #define STDIN 0  // file descriptor for standard input
 
 int main(void)
 {
-    struct timeval tv;
-    fd_set readfds;
 
-    tv.tv_sec = 7;
-    tv.tv_usec = 500000;
+  char buf[512];
+  int sret;
+  int bytes_read;
 
-    FD_ZERO(&readfds);
-    FD_SET(STDIN, &readfds);
+  struct timeval tv;
+  fd_set readfds;
 
-    // don't care about writefds and exceptfds:
-    select(STDIN+1, &readfds, NULL, NULL, &tv);
 
-    if (FD_ISSET(STDIN, &readfds))
-        printf("A key was pressed!\n");
-    else
+  while(1){
+
+      FD_ZERO(&readfds);
+      FD_SET(STDIN, &readfds);
+
+      tv.tv_sec = 7;
+      tv.tv_usec = 500000;
+
+      // don't care about writefds and exceptfds:
+      sret = select(STDIN+1, &readfds, NULL, NULL, &tv);
+
+      if(sret == 0){
         printf("Timed out.\n");
+      }
+      else{
+        memset(buf, 0, 512);
+        bytes_read = read(STDIN, buf, 512);
+        if(bytes_read == 0){
+          printf("Function terminated.\n");
+          break;
+        }
+        else{
+          printf("Le buffer a été activé !\n");
+          *(buf+strlen(buf)-1) = '\0';
+          printf("Buffer : %s\n", buf);
+        }
+      }
+    }
 
     return 0;
 }
